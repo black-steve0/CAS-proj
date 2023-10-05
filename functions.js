5a24351fe918781762bde1ffebf5f391d65d5341
black-steve0 <khalidhamada0708@gmail.com>
2023-10-05 18:43:05 +0300

    real code fr

---
+++ functions.js
@@ -0,0 +1,9 @@
+function togglePActions() {
+    if (document.getElementById("profile-actions").style.visibility == "hidden") {
+        document.getElementById("profile-actions").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("profile-actions").style.visibility = "hidden";
+    }
+    
+}
